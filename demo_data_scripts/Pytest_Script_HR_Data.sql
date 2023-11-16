74147a9678e14d349cacfd5a2e7127893ab36335
Avinash <avinashsekar045@gmail.com>
2023-11-16 11:53:40 +0530

    First Commit

---
+++ demo_data_scripts/Pytest_Script_HR_Data.sql
@@ -0,0 +1,148 @@
+
+/* Medium Article for DYNAMIC TABLE ==> https://medium.com/snowflake/4-new-table-types-in-2022-by-snowflake-a-summary-301fb4fcdf60 */
+
+use warehouse compute_wh;
+use role accountadmin;
+
+create or replace database hr_db;
+create or replace schema hr_schema;
+
+use database hr_db;
+use schema hr_schema;
+
+/* STAGE for "EMPLOYEE DETAILS" */
+create or replace stage hr_data_stage
+url = 's3://data-in-bucket/Human-Resources-Data-Set/'
+CREDENTIALS = (AWS_KEY_ID = 'AWS_KEY_ID'
+               AWS_SECRET_KEY = 'AWS_SECRET_KEY');
+
+
+/* FILE FORMAT */
+create or replace file format hr_file_fmt
+type = 'CSV'
+skip_header = 1
+field_delimiter = ','
+FIELD_OPTIONALLY_ENCLOSED_BY = '"';
+
+ls @hr_data_stage;
+
+select SPLIT_PART(metadata$filename, '/', 0) from @hr_data_stage (file_format => hr_file_fmt, pattern=>'.*Human Resources Data Set.*[.]csv');
+
+
+-- Create External Table without Defining Columns
+/*
+CREATE OR REPLACE EXTERNAL TABLE ibm_ext_tbl WITH
+LOCATION = @ibm_dataset
+FILE_FORMAT = ibm_file_format
+PATTERN='.*Human Resources Data Set.*[.]csv';
+*/
+
+-- Create External Table with Column Definition
+CREATE OR REPLACE EXTERNAL TABLE EMPLOYEE_EXTN (
+Employee_Name varchar            as (value:c1::varchar) ,
+EmpID int                        as (value:c2::int)     ,
+MarriedID int                    as (value:c3::int)     ,
+MaritalStatusID int              as (value:c4::int)     ,
+GenderID int                     as (value:c5::int)     ,
+EmpStatusID int                  as (value:c6::int)     ,
+DeptID int                       as (value:c7::int)     ,
+PerfScoreID int                  as (value:c8::int)     ,
+FromDiversityJobFairID int       as (value:c9::int)     ,
+Salary bigint                    as (value:c10::bigint) ,
+Termd int                        as (value:c11::int)    ,
+PositionID int                   as (value:c12::int)    ,
+Position varchar                 as (value:c13::varchar),
+State varchar                    as (value:c14::varchar),
+Zip bigint                       as (value:c15::bigint) ,
+DOB date                         as (value:c16::date)   ,
+Sex  varchar                     as (value:c17::varchar),
+MaritalDesc varchar              as (value:c18::varchar),
+CitizenDesc varchar              as (value:c19::varchar),
+HispanicLatino varchar           as (value:c20::varchar),
+RaceDesc varchar                 as (value:c21::varchar),
+DateofHire date                  as (value:c22::date)   ,
+DateofTermination date           as (value:c23::date)   ,
+TermReason varchar               as (value:c24::varchar),
+EmploymentStatus varchar         as (value:c25::varchar),
+Department varchar               as (value:c26::varchar),
+ManagerName varchar              as (value:c27::varchar),
+ManagerID int                    as (value:c28::int)    ,
+RecruitmentSource varchar        as (value:c29::varchar),
+PerformanceScore varchar         as (value:c30::varchar),
+EngagementSurvey float           as (value:c31::float)  ,
+EmpSatisfaction varchar          as (value:c32::varchar),
+SpecialProjectsCount int         as (value:c33::int)    ,
+LastPerformanceReview_Date date  as (value:c34::date)   ,
+DaysLateLast30 int               as (value:c35::int)    ,
+Absences int                     as (value:c36::int)
+)WITH
+LOCATION = @hr_data_stage
+FILE_FORMAT = hr_file_fmt
+PATTERN='.*Human Resources Data Set.*[.]csv';
+
+
+/* Employee Records from Cloud using COPY INTO ==> Consider as SOURCE */
+CREATE OR REPLACE TABLE employee_copy_tbl(
+Employee_Name varchar          ,
+EmpID int                      ,
+MarriedID int                  ,
+MaritalStatusID int            ,
+GenderID int                   ,
+EmpStatusID int                ,
+DeptID int                     ,
+PerfScoreID int                ,
+FromDiversityJobFairID int     ,
+Salary bigint                  ,
+Termd int                      ,
+PositionID int                 ,
+Position varchar               ,
+State varchar                  ,
+Zip bigint                     ,
+DOB date                       ,
+Sex  varchar                   ,
+MaritalDesc varchar            ,
+CitizenDesc varchar            ,
+HispanicLatino varchar         ,
+RaceDesc varchar               ,
+DateofHire date                ,
+DateofTermination date         ,
+TermReason varchar             ,
+EmploymentStatus varchar       ,
+Department varchar             ,
+ManagerName varchar            ,
+ManagerID int                  ,
+RecruitmentSource varchar      ,
+PerformanceScore varchar       ,
+EngagementSurvey float         ,
+EmpSatisfaction varchar        ,
+SpecialProjectsCount int       ,
+LastPerformanceReview_Date date,
+DaysLateLast30 int             ,
+Absences int  
+);
+
+-- Making Changes in SOURCE
+update employee_copy_tbl set LASTPERFORMANCEREVIEW_DATE = current_date() where empid in (10084,10196,10088);
+-- delete from ibm_det_copy_tbl where department like '%Research & Development%'; -- IN SRC but NOT IN Target
+
+COPY INTO employee_copy_tbl FROM @hr_data_stage
+FILE_FORMAT = hr_file_fmt
+PATTERN = '.*Human Resources Data Set.*[.]csv'
+FORCE = FALSE;
+
+
+/* Creating DYNAMIC TABLE => Combination of Stream and Task with Table*/
+CREATE OR REPLACE DYNAMIC TABLE EMPLOYEE_DETAILS_DYNAMIC
+TARGET_LAG = '10 minutes'
+WAREHOUSE = COMPUTE_WH AS
+select 
+SPLIT(TRIM(split(EMPLOYEE_NAME,',')[1]::varchar),' ')[0]::text as FIRSTNAME,TRIM(split(EMPLOYEE_NAME,',')[0]::varchar) as LASTNAME,
+-- EMPLOYEE_NAME,
+EMPID,SALARY,POSITION,STATE,ZIP,DOB,SEX,DATEOFHIRE,DATEOFTERMINATION,EMPLOYMENTSTATUS,DEPARTMENT,MANAGERNAME,RECRUITMENTSOURCE,PERFORMANCESCORE,LASTPERFORMANCEREVIEW_DATE 
+from employee_copy_tbl;
+
+-- Suspending the EMPLOYEE_DETAILS_DYNAMIC (DYNAMIC TABLE) to stop the SYNC
+alter dynamic table EMPLOYEE_DETAILS_DYNAMIC suspend;
+-- alter dynamic table EMPLOYEE_DETAILS_DYNAMIC resume;
+
+select * from EMPLOYEE_DETAILS_DYNAMIC;
---
+++ test_main.py
@@ -0,0 +1,134 @@
+from snowflake.snowpark import Session
+from snowflake.snowpark.dataframe import col
+import logging
+import logging.handlers
+import os
+import json
+
+
+
+# Parse the JSON string into a Python dictionary
+json_str = open('./config/config.json','r+').read()
+config_dict = json.loads(json_str)
+
+
+# pytest -v -s pytest_cicd.py
+# pytest -o log_cli=true -v -s pytest_cicd.py
+# pytest -o log_cli=true -v -s pytest_cicd.py
+# pytest --log-file=test_log.txt test_example.py
+
+# """
+# Remove or truncate the existing log file
+log_file = "pytest.log"
+if os.path.exists(log_file):
+    with open(log_file, "w"):  # Open the file in "write" mode, which truncates it
+        pass
+
+# Create and configure logger
+logger = logging.getLogger(__name__) # Github Stored in Log as UTC Timezone | Example => IST : 4:13PM UTC : 10:43 AM (-5.5 hrs)
+logger.setLevel(logging.DEBUG)
+logger_file_handler = logging.handlers.RotatingFileHandler(
+    filename="pytest.log",
+    maxBytes=1024 * 1024,
+    backupCount=1,
+    encoding="utf8",
+)
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+logger_file_handler.setFormatter(formatter)
+logger.addHandler(logger_file_handler)
+
+# logger = setup_logger()
+
+user = os.environ.get("snow_user")
+password = os.environ.get("snow_pwd")
+account = os.environ.get("snow_acc")
+warehouse = config_dict['warehouse']
+role = config_dict['role']
+
+
+connection_parameters = {
+    "user": user,
+    "password" : password,
+    "account": account,
+    "role": role,
+    "warehouse": warehouse
+ }
+
+session = Session.builder.configs(connection_parameters).create()
+
+session.use_database(database=config_dict['database']);
+session.use_schema(schema=config_dict['schema']);
+session.use_warehouse(warehouse=config_dict['warehouse']);
+
+file_path = config_dict['src'];
+file_content = open(file=file_path,mode='r+');
+query = file_content.read();
+# print(query)
+
+# """
+# '''
+# source & target CHECK
+src = session.sql(query=query.replace(';',''));
+target = session.table(name=config_dict['target']);
+
+# INPUT for Getting COLUMNS LIST
+# columns_list = input("Enter the column list with ',' seperated : ").split(',')
+columns_list = config_dict['columns_list_duplicates_or_not'].split(',')
+list_of_cols = [col_.strip() for col_ in columns_list]
+
+columns_list_null_check = config_dict["columns_list_null_check"].split(',')
+list_of_cols_nc = [col_.strip() for col_ in columns_list]
+
+def test_rowcount():
+    logger.info(f"ROWCOUNT => The count of Source : {src.count()} and Target : {target.count()} Matching : {src.count() == target.count()}")
+    assert src.count() == target.count()
+
+def test_data_mismatch():
+    mismatch_cols = []
+    matched_cols = [tgt_col for src_col,tgt_col in zip(sorted(src.columns),sorted(target.columns)) if src_col == tgt_col]
+    result = src.select(matched_cols).minus(target.select(matched_cols)).collect()
+    log_res = 'PASSED' if not bool(result) == True else 'NOT PASSED'
+    logger.info(f"DATA MISMATCH : {log_res}")
+    assert not bool(result) == True,f"The Mismatching count is {len(mismatch_cols)} \n and the columns are {','.join(mismatch_cols)}"
+
+def test_duplicates_or_not():
+    # EMPLOYEE_NUMBER, EMPLOYEE_FULL_NAME, EMPLOYEE_STATUS_CODE, DEPARTMENT_NUMBER, SUPERVISOR_NUMBER
+    # EMPLOYEE_NUMBER, EMPLOYEE_FULL_NAME, EMPLOYEE_STATUS_CODE, DEPARTMENT_NUMBER # current one
+    # EMPID,DEPARTMENT,POSITION
+    result = target.select(list_of_cols).group_by(list_of_cols).count().filter(col('count') > 1).collect() # []
+    log_res = 'PASSED' if not bool(result) == True else 'NOT PASSED'
+    logger.info(f"DUPLICATES OR NOT : {log_res}")
+    assert not bool(result) == True
+
+def test_null_check():
+    null_columns = ','.join([
+    col_ for col_ in target.select(list_of_cols_nc).columns if target.select(list_of_cols_nc).where(target.select(list_of_cols_nc).col(col_).is_null()).collect()
+    ])
+    log_res = 'PASSED' if target.count() == target.select(list_of_cols_nc).dropna().count() else 'NOT PASSED'
+    logger.info(f"NULL CHECK : {log_res}")
+    assert True if target.count() == target.select(list_of_cols_nc).dropna().count() else False, f"The Null Columns are {null_columns}"
+
+def test_column_match_sttm():
+    count_ = 0
+    col_in_tgt_not_in_sttm = []
+    sttm_table = session.table(name=config_dict["sttm_table"]).select("COLUMN NAME").collect();
+    sttm_table_col_list = [f"{cols[0]}".lower() for cols in sttm_table]
+    for column_ in target.columns:
+        if f"{column_}".lower() in sttm_table_col_list:
+            count_ += 1
+        if f"{column_}".lower() not in sttm_table_col_list:
+                col_in_tgt_not_in_sttm.append(column_)
+
+    cols_in_sttm_not_in_tgt = [col_name for col_name in sttm_table_col_list if col_name not in target.columns]
+ 
+    if len(cols_in_sttm_not_in_tgt) != 0:
+        result_ = f"COLUMN PRESENT IN STTM BUT NOT IN TARGET : {','.join(cols_in_sttm_not_in_tgt)}"
+    
+    if count_ != len(sttm_table_col_list):
+        res_ = f"Columns present in Target not in STTM {','.join(col_in_tgt_not_in_sttm)}"
+
+    log_res = 'PASSED' if count_ == len(sttm_table_col_list) else 'NOT PASSED'
+    logger.info(f"COLUMN MATCH WITH STTM :  {log_res}")
+    assert count_ == len(sttm_table_col_list),f"{res_} {result_}"
+
+# '''
